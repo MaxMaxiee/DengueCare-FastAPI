f11c8a695b28d9aa6d0dc9463bf49c2061e4cbd0
MaxMaxiee <albamaxine99@gmail.com>
2024-03-21 13:05:56 +0800

    auth

--- db/db_user.py
+++ db/db_user.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from schemas import UserBase
 from db.models import DbUser
 from db.hash import Hash
+from fastapi import HTTPException, status
 
 
 def create_user(db: Session, request: UserBase):
@@ -23,3 +24,44 @@ def create_user(db: Session, request: UserBase):
 
 def get_all_user(db: Session):
     return db.query(DbUser).all()
+
+def get_user_by_id(db: Session, id: str):
+    user = db.query(DbUser).filter(DbUser.id.contains(id)).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {id} not found')
+
+def get_user_by_username(db: Session, username: str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with username {username} not found')
+    return user
+
+def update_user_by_id(db: Session, id:str, request:DbUser):
+    user = db.query(DbUser).filter(DbUser.id.contains(id))
+    if not user.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {id} not found')
+    user.update({
+        DbUser.age: request.age,
+        DbUser.contact_number: request.contact_number,
+        DbUser.email: request.email,
+        DbUser.firstname: request.firstname,
+        DbUser.lastname: request.lastname,
+        DbUser.purok: request.purok,
+        DbUser.password: Hash.bcrypt(request.password),
+        DbUser.role: request.role,
+        DbUser.sex: request.sex,
+    })
+    db.commit()
+    return user
+
+def delete_user_by_id(db: Session, id: str):
+    user = db.query(DbUser).filter(DbUser.id.contains(id)).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {id} not found')
+    db.delete(user)
+    db.commit()
+    return {'message': f'User with id {id} has been deleted'}
--- router/user_router.py
+++ router/user_router.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_user
 from typing import List
+from auth.oauth2 import get_current_user
 
 
 router = APIRouter(
@@ -18,5 +19,26 @@ def create_user(request: UserBase, db: Session = Depends(get_db)):
 
 # Router for getting all users
 @router.get('/', response_model=List[UserDisplay])
-def get_all_user(db:Session = Depends(get_db)):
+def get_all_user(db:Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_user.get_all_user(db)
+
+# Router for getting a specif user using ID
+@router.get('/{id}', response_model=UserBase)
+def get_user_by_id(id: str, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
+    return db_user.get_user_by_id(db, id)
+
+# Router for updating user info
+@router.put('/{id}', response_model=UserBase)
+def update_user_by_id(id: str, request: UserBase, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
+    return db_user.update_user_by_id(db, id, request)
+
+@router.delete('/{id}')
+def delete_user_by_id(id: str, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
+    return db_user.delete_user_by_id(db, id)
+
+
+
+
+
+
+
